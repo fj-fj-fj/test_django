83853e38d85ad2ce61bbc04ecbdce6fce19c4156
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-05-14 12:35:36 +0300

    created model post

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def post_lists(request):
-    return HttpResponse('Hello World')
+    return render(request, 'firstapp/index.html')

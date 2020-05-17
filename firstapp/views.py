20fcf81a391d31de2cd92191fe37a6b7d7ba1be5
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-05-17 14:48:02 +0300

    created view tats_list, tag_detail and html

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post, Tag
 
 
 def posts_list(request):
@@ -11,3 +11,13 @@ def posts_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'firstapp/post_detail.html', {'post': post})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'firstapp/tags_list.html', {'tags': tags})
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'firstapp/tag_detail.html', {'tag': tag})

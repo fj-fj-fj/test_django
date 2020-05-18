c2a34767914de1d87a1b1b4ac4cfa1fa14bbc0dc
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-05-18 12:33:40 +0300

    changed detail views to classes based views

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
 
 from .models import Post, Tag
 
@@ -8,9 +10,10 @@ def posts_list(request):
     return render(request, 'firstapp/index.html', {'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'firstapp/post_detail.html', {'post': post})
+class PostDetail(View):
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        return render(request, 'firstapp/post_detail.html', {'post': post})
 
 
 def tags_list(request):
@@ -18,6 +21,7 @@ def tags_list(request):
     return render(request, 'firstapp/tags_list.html', {'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'firstapp/tag_detail.html', {'tag': tag})
+class TagDetail(View):
+    def get(self, request, slug):
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        return render(request, 'firstapp/tag_detail.html', {'tag': tag})

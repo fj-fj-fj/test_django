cdfcb6f30d50c945dfb191124ea8c638286fd770
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-05-22 13:27:48 +0300

    added forms - postform, view - postcreate, tmp - post_create.html

--- firstapp/views.py
+++ firstapp/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .forms import TagForm, PostForm
 
 
 def posts_list(request):
@@ -16,6 +16,20 @@ def posts_list(request):
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'firstapp/post_detail.html'
+
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'firstapp/post_create.html', {'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'firstapp/post_create.html', {'form': bound_form})
 
 
 def tags_list(request):

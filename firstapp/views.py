c28201c26212903a18f06a2ff88141aded2348df
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-05-26 11:05:48 +0300

    delete post/tag

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
@@ -28,6 +29,18 @@ class PostUpdate(ObjectUpdateMixin, View):
     model_form = PostForm
     template = 'firstapp/post_update_form.html'
 
+
+class PostDelete(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        return render(request, 'firstapp/post_delete_form.html', {'post': post})
+
+    def post(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        post.delete()
+        return redirect(reverse('posts_list_url'))
+
+
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'firstapp/tags_list.html', {'tags': tags})
@@ -47,3 +60,14 @@ class TagUpdate(ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'firstapp/tag_update_form.html'
+
+
+class TagDelete(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        return render(request, 'firstapp/tag_delete_form.html', {'tag': tag})
+
+    def post(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        tag.delete()
+        return redirect(reverse('tags_list_url'))

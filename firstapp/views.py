b8f7b95387cc63204eb78465c64e0388fb2979a8
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-05-29 12:31:05 +0300

    hidden update, create, delete

--- firstapp/views.py
+++ firstapp/views.py
@@ -7,6 +7,8 @@ from .forms import PostForm, TagForm
 from .utils import (ObjectDetailMixin, ObjectCreateMixin,
                     ObjectUpdateMixin, ObjectDeleteMixin)
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -18,21 +20,24 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'firstapp/post_detail.html'
 
 
-class PostCreate(ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model_form = PostForm
     template = 'firstapp/post_create.html'
+    raise_exeption = True
 
 
-class PostUpdate(ObjectUpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Post
     model_form = PostForm
     template = 'firstapp/post_update_form.html'
+    raise_exeption = True
 
 
-class PostDelete(ObjectDeleteMixin, View):
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Post
     template = 'firstapp/post_delete_form.html'
     redirect_url = 'posts_list_url'
+    raise_exeption = True
 
 
 def tags_list(request):
@@ -45,18 +50,21 @@ class TagDetail(ObjectDetailMixin, View):
     template = 'firstapp/tag_detail.html'
 
 
-class TagCreate(ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model_form = TagForm
     template = 'firstapp/tag_create.html'
+    raise_exeption = True
 
 
-class TagUpdate(ObjectUpdateMixin, View):
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'firstapp/tag_update_form.html'
+    raise_exeption = True
 
 
-class TagDelete(ObjectDeleteMixin, View):
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Tag
     template = 'firstapp/tag_delete_form.html'
     redirect_url = 'tags_list_url'
+    raise_exeption = True

78000cce1a12a54dba5ddd4313931b59f3198963
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-07-17 22:40:35 +0300

    rus admin

--- firstapp/models.py
+++ firstapp/models.py
@@ -7,21 +7,13 @@ def gen_slug(s):
     new_slug = slugify(s, allow_unicode=True)
     return new_slug + '-' + str(int(time()))
 
-from django.utils.text import slugify
-from time import time
-
-
-def  gen_slug(s):
-    new_slug = slugify(s, allow_unicode=True)
-    return new_slug + '-' + str(int(time()))
-
 
 class Post(models.Model):
-    title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, blank=True, unique=True)
-    body = models.TextField(blank=True, db_index=True)
+    title = models.CharField('Заголовок поста', max_length=150, db_index=True)
+    slug = models.SlugField('Слаг', max_length=150, blank=True, unique=True)
+    body = models.TextField('Текст поста', blank=True, db_index=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
-    date_pub = models.DateTimeField(auto_now_add=True)
+    date_pub = models.DateTimeField('Дата публикации', auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
@@ -47,11 +39,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-date_pub']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50, unique=True)
+    title = models.CharField('Заголовок тэга', max_length=50)
+    slug = models.SlugField('Слаг тэга', max_length=50, unique=True)
 
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
@@ -67,12 +61,18 @@ class Tag(models.Model):
 
     class Meta:
         ordering = ['title']
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete = models.CASCADE)
-    author = models.CharField(max_length=50)
-    text = models.CharField(max_length=200)
+    author = models.CharField('Имя автора', max_length=50)
+    text = models.CharField('Текст комментария', max_length=200)
 
     def __str__(self):
         return self.author
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'

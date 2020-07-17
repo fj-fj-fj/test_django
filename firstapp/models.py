206e763d3ddf140a394e417f8db40f727d72e171
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-07-17 14:19:26 +0300

    create comment model; register models in admin

--- firstapp/models.py
+++ firstapp/models.py
@@ -67,3 +67,12 @@ class Tag(models.Model):
 
     class Meta:
         ordering = ['title']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    author = models.CharField(max_length=50)
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.author

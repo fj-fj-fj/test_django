4572f93967b06805a66e7da28dab568a7cff11a4
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-11-19 20:59:34 +0300

    mod: settings, staticfiles

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -125,12 +125,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'run', 'static_root')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'run', 'static_root')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'run', 'media_root')
 MEDIA_URL = '/media/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -144,7 +144,8 @@ LOGIN_REDIRECT_URL = '/'
 # LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' # gzip
+# storage backend
 django_heroku.settings(locals())
 
 ASGI_APPLICATION = 'blog.routing.application'
